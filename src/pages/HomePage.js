4909456cc2f9c166da7a44dd8abc0f33b2b1a7e5
meisam <maghsoodiradmeisam12@gmail.com>
2022-04-09 23:57:53 +0430

    add products to HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,26 @@
 import Layout from "../Layout/Layout";
+import * as data from "../data";
 
 const HomePage = () => {
   return (
     <Layout>
-      <h2>this is homePage</h2>
+      <main className="container">
+        <section className="productList">
+          {data.products.map((p) => {
+            return (
+              <section className="product" key={p.name}>
+                <div className="productImg">
+                  <img src={p.image} alt={p.name} />
+                </div>
+                <div className="productDesc">
+                  <p>{p.name}</p>
+                  <p>$ {p.price}</p>
+                </div>
+              </section>
+            );
+          })}
+        </section>
+      </main>
     </Layout>
   );
 };
