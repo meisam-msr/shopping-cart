bc84547a830a825ab145f61898cf4da824d9df82
meisam <maghsoodiradmeisam12@gmail.com>
2022-04-09 19:36:30 +0430

    HomePage added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <HomePage />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
