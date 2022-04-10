127ac63f353341cab65f9aee8fe40e8eec2f8ff1
meisam <maghsoodiradmeisam12@gmail.com>
2022-04-11 00:35:40 +0430

    cartPage updated

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,9 +1,45 @@
 import Layout from "../Layout/Layout";
+import { useCart } from "../providers/CartProvider";
+import "./cartPage.css";
 
 const CartPage = () => {
+  const { cart } = useCart();
+  console.log(cart);
+
+  if (!cart.length)
+    return (
+      <Layout>
+        <main>
+          <h2>cart is empty !</h2>
+        </main>
+      </Layout>
+    );
+
   return (
     <Layout>
-      <main>this is cart page</main>
+      <main className="container">
+        <section className="cartCenter">
+          <section className="cartItemList">
+            {cart.map((item) => {
+              return (
+                <div className="cartItem" key={item.id}>
+                  <div className="itemImg">
+                    <img src={item.image} alt={item.name} />
+                  </div>
+                  <div>{item.name}</div>
+                  <div>{item.price * item.quantity}</div>
+                  <div>
+                    <button>Remove</button>
+                    <button>{item.quantity}</button>
+                    <button>Add</button>
+                  </div>
+                </div>
+              );
+            })}
+          </section>
+          <section className="cartSummary">cart summary</section>
+        </section>
+      </main>
     </Layout>
   );
 };
